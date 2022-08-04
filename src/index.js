9c8af7a524870b1a8fcab06fc8b43d9052c9bf1e
ifechukwudei <nwokoifechukwu@gmail.com>
2022-08-04 01:18:48 +0100

    modified package.json, index.html and fetched weather api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-const apiKey = process.env.API_KEY
+const apiKey = "b92f6c8cab2bec8789ee1cca2ee645cc"
 
-console.log(apiKey)
+async function fetchCity(city) {
+  const apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    apiKey
+  const response = await fetch(apiUrl)
+  const weather = await response.json()
+  return weather
+}
+/*let weather = {
+  apikey: "b92f6c8cab2bec8789ee1cca2ee645cc",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=" +
+        this.apikey
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  },
+}*/
