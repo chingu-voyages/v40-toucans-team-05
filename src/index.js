20fa7d89d5769957b795382ad6461e033901033b
fmtabbara <fmtabbara@hotmail.co.uk>
2022-08-11 17:22:39 +0100

    update desctructuring

--- src/index.js
+++ src/index.js
@@ -29,20 +29,22 @@ inputBar.addEventListener("keydown", ({ key }) => {
   }
 })*/
 async function displayWeather(data) {
-  const { name } = data
-  const { temp } = data.temp
-  // convert temp from kelvin to celsuis.
-  //temp = data.temp - 273.15
-  const { humidity } = data.humidity
-  const { icon, description } = data.weather[0]
-  const { speed } = data.speed
+  const {
+    name,
+    weather,
+    main: { humidity, temp },
+    wind: { speed },
+  } = data
+  const celcius = Math.round(temp - 273.15)
+  const { icon, description } = weather[0]
+
   console.log(name, temp, humidity, icon, description, speed)
   document.querySelector(".city").innerText = `Weather in ${name}`
   document.querySelector(
     ".icon"
   ).src = `http://openweathermap.org/img/wn/${icon}@2x.png`
   document.querySelector(".description").innerText = description
-  document.querySelector(".temperature").innerText = `${temperature}°C`
+  document.querySelector(".temperature").innerText = `${celcius}°C`
   document.querySelector(".humidity").innerText = `Humidity ${humidity} %`
   document.querySelector(".wind-speed").innerText = `wind speed ${speed}km`
 }
@@ -66,6 +68,7 @@ async function fetchCity(city) {
 
     if (response.status === 200) {
       console.log(data)
+      displayWeather(data)
     } else {
       errorBar.classList.add("show")
       errorText.innerText += "Error: " + data.message
